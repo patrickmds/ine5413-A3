7431f9775c76ad1e2c13a4bd1cdd209563481d0b
Rodrigo Garcia <rodrigo.mellogarcia@bridge.ufsc.br>
2020-12-13 03:13:54 -0300

    hopcroft-karp pronto

--- algoritmos.py
+++ algoritmos.py
@@ -45,15 +45,18 @@ def edmonds_karp(grafo, v_inicial, v_sorvedouro):
 def hopcroft_karp(grafo):
     distancias = [float("inf") for x in range(len(grafo.vertices))]
     mates = [None for x in range(len(grafo.vertices))]
+    distNone = [0]
 
-    busca_em_largura_emparelhamento(grafo, mates, distancias)
+    X = (int)(grafo.qtd_vertices()/2 - 1)
+    m = 0
+    while(busca_em_largura_emparelhamento(grafo, mates, distancias, distNone)):
+        for x in range(X+1):
+            if mates[x] == None:
+                if busca_em_profundidade_emparelhamento(grafo, mates, x, distancias, distNone):
+                    m += 1
 
-
-
-
-
-
-    return
+    print(f"Emparelhamento máximo: {m}")
+    return mates
 
 def coloracao(grafo):
     return
@@ -77,13 +80,10 @@ def busca_em_largura_fluxo(grafo, v_inicial, v_sorvedouro, rede_residual):
                 antecessores[v] = u
                 
                 if v == v_sorvedouro:
-                    # print("Sorvedouro encontrado. Criando caminho aumentante...")
                     caminho_aumentante = []
                     while v != None:
                         caminho_aumentante.append(v)
-                        # print(f"{v}", end="-")
                         v = antecessores[v]
-                    # print("end")
                     return caminho_aumentante
                 
                 fila.append(v)
@@ -92,8 +92,6 @@ def busca_em_largura_fluxo(grafo, v_inicial, v_sorvedouro, rede_residual):
     return None
 
 def minimo(valores):
-
-
     if valores == None or len(valores)<1:
         print("Erro ao retornar valor mínimo")
         return None
@@ -106,19 +104,53 @@ def minimo(valores):
     return resultado
 
 
-def busca_em_largura_emparelhamento(grafo, mates, distancias):
+def busca_em_largura_emparelhamento(grafo, mates, distancias, distNone):
     Q = []
     X = (int)(grafo.qtd_vertices()/2 - 1)
-    
-    print(f"X: de 0 a {X}")
+
+    INF = float("inf")
 
     for x in range(X+1):
         if mates[x]== None:
             distancias[x] = 0
             Q.append(x)
         else:
-            distancias[x] = (int)(float("inf"))
+            distancias[x] = INF
 
-    return
+    distNone[0] = INF
+    while len(Q) > 0:
+        x = Q.pop()
+        if distancias[x] < distNone[0]: 
+            for y in grafo.vizinhos(x):
+                if mates[y] == None:
+                    if distNone[0] == INF:
+                        distNone[0] = distancias[x] + 1
+                else:
+                    if distancias[mates[y]] == INF:
+                        distancias[mates[y]] = distancias[x] + 1
+                        Q.append(mates[y])
 
+    return distNone[0] != INF      
+
+def busca_em_profundidade_emparelhamento(grafo, mates, x, distancias, distNone):
+    INF = float("inf")
+    
+    if x != None:
+        for y in grafo.vizinhos(x):
+            if mates[y] == None:
+                if distNone[0] == distancias[x] + 1:
+                    if busca_em_profundidade_emparelhamento(grafo, mates, mates[y], distancias, distNone):
+                        # mates[y] = x
+                        mates[x] = y
+                        return True
+            else:
+                if distancias[mates[y]] == distancias[x] + 1:
+                    if busca_em_profundidade_emparelhamento(grafo, mates, mates[y], distancias, distNone):
+                        mates[y] = x
+                        mates[x] = y
+                        return True
+        distancias[x] = INF
+        return False
+
+    return True
 
